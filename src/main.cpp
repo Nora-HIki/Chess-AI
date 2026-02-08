fccd939b39a09edb9a9474773cfdee6e01b0a847
yatogami <brookself667@slmail.me>
2026-02-09 02:19:59 +0530

    displayed a window

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,40 @@
-#include <iostream>
-#include <SDL3/SDL.h>
 #include "Setup.hpp"
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_events.h>
+#include <SDL3/SDL_init.h>
+#include <SDL3/SDL_render.h>
+#include <SDL3/SDL_video.h>
+#include <iostream>
 #include "Board.hpp"
 
-int main (int argc, char *argv[]) {
-    
-  setup_board();
+void cleanup(SDL_Window *win,SDL_Renderer *renderer);
+
+int main(int argc, char *argv[]) {
+  int h = 800;
+  int w = 1200;
   SDL_Init(SDL_INIT_VIDEO);
-  SDL_Quit();
-
-  std::cout << "bankai" << std::endl;
-  
-
-
-
-  Board_layout board1;
+  SDL_Window *win = SDL_CreateWindow("Chess", w, h, 0);
+  SDL_Renderer *renderer = SDL_CreateRenderer(win,nullptr);
+  bool running=true;
+  while(running){
+    std::cout<<"running"<<std::endl;
+    SDL_Event evento={0};
+    SDL_PollEvent(&evento);
+    switch (evento.type){
+      case SDL_EVENT_QUIT:
+        running=false;
+        break;
+    }
+    SDL_SetRenderDrawColor(renderer,244,134,234,10);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+  }
+  cleanup(win,renderer);
   return 0;
+}
+
+void cleanup(SDL_Window *win,SDL_Renderer *renderer) {
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(win);
+  SDL_Quit();
 }
